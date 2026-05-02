acc3a0007e3488f353592bf6a127ba14b33e4083
depo0749 <depo0749@gmail.com>
2026-05-02 15:46:31 +0300

    Add KutuphanMVC/Controllers/HomeController.cs

---
+++ KutuphanMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using KutuphanMVC.Models;
+
+namespace KutuphanMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Anasayfa()
+    {
+        return View();
+    }
+
+    public IActionResult Hakkimizda()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
